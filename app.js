a52ce1f9ee0aae501579d14db9ea88a11b9a58a3
anichu <anis.cse5.bu@gmail.com>
2022-08-22 14:10:37 +0600

    recap the code

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const playersListElement = document.getElementById("players-list");
 const cardsElement = document.getElementById("cards");
 
+function addPlayersToList(selectedElement) {
+	let playerName = selectedElement.parentNode.children[0].innerText;
+	let createListElement = document.createElement("li");
+	createListElement.classList.add("py-2");
+	createListElement.innerText = playerName;
+	playersListElement.appendChild(createListElement);
+	selectedElement.disabled = true;
+}
+
 cardsElement.addEventListener("click", (event) => {
 	let selectedElement = event.target;
 	let selectedText = event.target.innerText;
 	let listElementsLength = playersListElement.children.length;
 	if (selectedText === "Select") {
 		if (listElementsLength < 5) {
-			let playerName = selectedElement.parentNode.children[0].innerText;
-			let createListElement = document.createElement("li");
-			createListElement.classList.add("py-2");
-			createListElement.innerText = playerName;
-			playersListElement.appendChild(createListElement);
-			selectedElement.disabled = true;
+			addPlayersToList(selectedElement);
 		} else {
 			alert("You have already chosen five players");
 		}
